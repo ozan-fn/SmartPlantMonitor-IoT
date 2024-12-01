5b8a1e3a7388c00de2c01c70c928575901b9d6ef
ozan-fn <23SA11A032@students.amikompurwokerto.ac.id>
2024-12-01 10:56:45 +0700

    y

--- src/components/Content/Team.tsx
+++ src/components/Content/Team.tsx
@@ -1,6 +1,8 @@
+import { InstagramIcon } from "lucide-react";
+
 export default function Team() {
     return (
-        <div className="flex-1 mt-12 bg-zinc-900 text-white rounded-lg shadow-lg">
+        <div className="flex-1 mt-12 bg-zinc-900 text-white rounded-lg shadow-lg overflow-auto">
             <h2 className="text-3xl font-semibold text-center mb-6">Anggota Tim 4Trio</h2>
             <table className="table w-full text-lg text-left">
                 <thead>
@@ -13,29 +15,43 @@ export default function Team() {
                     <tr className="border-t border-zinc-700">
                         <td className="p-2 capitalize">AKHMAD FAUZAN</td>
                         <td className="p-2">
-                            <a href="">ozan.fn</a>
+                            <a className="flex flex-row items-center gap-2" href="https://www.instagram.com/ozan.fn">
+                                <InstagramIcon />
+                                ozan.fn
+                            </a>
                         </td>
                     </tr>
                     <tr className="border-t border-zinc-700">
                         <td className="p-2 capitalize">MUHAMMAD AGUS PRIYANTO</td>
                         <td className="p-2 cursor-pointer">
-                            <a href="">Rheiyn._</a>
+                            <a className="flex flex-row items-center gap-2" href="https://www.instagram.com/Rheiyn._">
+                                <InstagramIcon />
+                                Rheiyn._
+                            </a>
                         </td>
                     </tr>
                     <tr className="border-t border-zinc-700">
                         <td className="p-2 capitalize">Firman zamzami aziz</td>
                         <td className="p-2">
-                            <a href="">Gw_firman</a>
+                            <a className="flex flex-row items-center gap-2" href="https://www.instagram.com/Gw_firman">
+                                <InstagramIcon />
+                                Gw_firman
+                            </a>
                         </td>
                     </tr>
                     <tr className="border-t border-zinc-700">
                         <td className="p-2 capitalize">Muhammad Zhiya Ulhaq</td>
                         <td className="p-2">
-                            <a href="">Zhiya.Ulhaq</a>
+                            <a className="flex flex-row items-center gap-2" href="https://www.instagram.com/Zhiya.Ulhaq">
+                                <InstagramIcon />
+                                Zhiya.Ulhaq
+                            </a>
                         </td>
                     </tr>
                 </tbody>
             </table>
+
+            <div className="h-20 w-1" />
         </div>
     );
 }
