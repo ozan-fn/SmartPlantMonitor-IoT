1f90fa923c332880cd4e6ed7a45b09621839fd57
Zhiya Ulhaq <zhiyaulhaq25@gmail.com>
2026-05-26 16:06:13 +0700

    Add water capacity metric and MQTT handling

--- src/mqttClient.ts
+++ src/mqttClient.ts
@@ -1,19 +1,28 @@
-import mqtt from "mqtt";
+import mqtt from 'mqtt'
 
-const mqttClient = mqtt.connect("wss://a93bb6a45b2d4a60bbf254a374d0e89f.s1.eu.hivemq.cloud:8884/mqtt", {
-  protocol: "wss",
-  clientId: `mqtt_${Math.random().toString(16).slice(3)}`,
-  username: "ozan6825", // Ganti dengan username Anda
-  password: "Akhmad6825", // Ganti dengan password Anda
-});
+const mqttClient = mqtt.connect(
+  'wss://a93bb6a45b2d4a60bbf254a374d0e89f.s1.eu.hivemq.cloud:8884/mqtt',
+  {
+    protocol: 'wss',
+    clientId: `mqtt_${Math.random().toString(16).slice(3)}`,
+    username: 'ozan6825', // Ganti dengan username Anda
+    password: 'Akhmad6825', // Ganti dengan password Anda
+  },
+)
 
-mqttClient.on("connect", () => {
-  console.log("Connected to MQTT broker");
-  mqttClient.subscribe(["ping", "suhu", "kelembaban", "kelembaban_tanah"]);
-});
+mqttClient.on('connect', () => {
+  console.log('Connected to MQTT broker')
+  mqttClient.subscribe([
+    'ping',
+    'suhu',
+    'kelembaban',
+    'kelembaban_tanah',
+    'kapasitas_air',
+  ])
+})
 
-mqttClient.on("error", (error: Error) => {
-  console.error("Connection failed:", error);
-});
+mqttClient.on('error', (error: Error) => {
+  console.error('Connection failed:', error)
+})
 
-export default mqttClient;
+export default mqttClient
